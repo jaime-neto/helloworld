78348cd2ffdbf4a290b8b7c4ab694b2ca03f4445
Jaime Neto <jaime.neto99@hotmail.com>
2019-09-23 08:45:48 -0300

    O usuario agora digita a mensagem que ira aparecer

--- hw.cpp
+++ hw.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 using namespace std;
 
 void mostrarMensagem(){
-    cout << "Hello, world!" << endl;
+    char s[10];
+    cin >> s;
+    cout << s << endl;
 }
 
 int main(void){
